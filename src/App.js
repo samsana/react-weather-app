c9cf176c5e37ffd2e1fa7e4eeec8a6fd235c3e6d
samsana <65514037+samsana@users.noreply.github.com>
2020-06-13 15:29:14 +0200

    added weather icon with license & credits

--- src/App.js
+++ src/App.js
@@ -32,6 +32,33 @@ function App() {
             Netlify.
           </a>
         </footer>
+        <small>
+          ICONS:{" "}
+          <a
+            href="https://www.iconfinder.com/iconsets/weather-filled-outline-6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            "Weather (filled outline)"
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.iconfinder.com/tes_azhari22"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tesyar azhari
+          </a>
+          , licensed under{" "}
+          <a
+            href="https://creativecommons.org/licenses/by/3.0/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            CC 3.0.
+          </a>
+        </small>
       </div>
     </Draggable>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Emoji from "./Emoji";
+import sun from "./img/sun.svg";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./Weather.css";
@@ -9,19 +10,21 @@ export default function Weather(props) {
     <div>
       <h3>Frankfurt am Main, DE</h3>
       <div className="row">
-        <div className="col-5">
+        <div className="col-4">
           <h6>
             <Emoji icon="📡" /> Clear sky
           </h6>
-          <h1>
-            19
-            <span className="Units">
-              <a href="/">°C</a>|<a href="/">°F</a>{" "}
-              <span className="WeatherIcon">
-                <Emoji icon="☀️" />
+          <div className="row">
+            <h1>
+              19
+              <span className="Units">
+                <a href="/">°C</a>|<a href="/">°F</a>{" "}
               </span>
-            </span>
-          </h1>
+            </h1>
+          </div>
+        </div>
+        <div className="col-3">
+          <img src={sun} alt="sun" />
         </div>
         <ul>
           <li>☞ Humidity: 36%</li>
